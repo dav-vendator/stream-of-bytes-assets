5180717d95a85037d23385e32437a9fe08621142
Dav Vendator <eruditoguerrero@gmail.com>
2018-11-30 14:46:45 +0530

    Update iinlink.js

--- particle/iinlink.js
+++ particle/iinlink.js
@@ -50,7 +50,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#0000ff",
         "opacity": 0.4,
         "width": 1
       },
